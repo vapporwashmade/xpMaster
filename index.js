e36fd0a31034358a39f6bf1b9550eaa7c8d1c72f
Apoorv Mhaswade <vappor.washmade@gmail.com>
2018-10-24 11:47:14 -0700

    discord bot

---
+++ index.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js');
+const config = require("./botconfig.json");
+const bot = new Discord.Client();
+const token = 'NTA0NDQxODM2ODg3MjEyMDM0.DrFXPA.9oSQpWKSRMKJToxM3AfjwxnkmsU';
+bot.on("message", function (message) {
+	if (message.author.bot || message.content.indexOf(config.prefix) !== 0) {
+		return;
+	}
+	if (message.content.toLowerCase() === 'hi') {
+		bot.reply("Hello, how are you doing?");
+	}
+});
+
+bot.login(token);
