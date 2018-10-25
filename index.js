e855dda9d9714e2460c0115929fb8c802097d3dd
Apoorv Mhaswade <vappor.washmade@gmail.com>
2018-10-25 15:12:30 -0700

    updated

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
+// add bot to server url:
+// https://discordapp.com/api/oauth2/authorize?client_id=504441836887212034&scope=bot&permissions=8
 const Discord = require('discord.js');
 const config = require("./botconfig.json");
 const bot = new Discord.Client();
 const token = 'NTA0NDQxODM2ODg3MjEyMDM0.DrFXPA.9oSQpWKSRMKJToxM3AfjwxnkmsU';
 bot.on("message", function (message) {
-	if (message.content.toLowerCase() === 'hi') {
-		bot.reply("Hello, how are you doing?");
+	if (message.content === 'hi') {
+		message.reply("Hello, how are you doing?");
 	}
 });
-
+bot.on('ready', function () {
+	console.log('ready');
+});
 bot.login(token);
