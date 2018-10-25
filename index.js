a4897c9ce32b2c5dbd8afce25e66816e68e7f3f3
Apoorv Mhaswade <vappor.washmade@gmail.com>
2018-10-25 16:46:28 -0700

    updated

--- index.js
+++ index.js
@@ -8,6 +8,21 @@ bot.on("message", function (message) {
 	if (message.content.toLowerCase() === 'hi') {
 		message.reply("Hello, how are you doing?");
 	}
+	var args = message.content.split(' ');
+	var command = args[1].toLowerCase();
+	if (message.author.bot || args.indexOf(config.prefix) !== 0) {
+		return;
+	}
+	if (command === 'say') {
+		if (args[2] === undefined) {
+			message.channel.send('Error: Need something to say');
+			return;
+		}
+		args.splice(0, 2);
+		message.channel.send(args.join(' '));
+	} else if (command === 'fail') {
+
+	}
 });
 bot.on('ready', function () {
 	console.log('ready');
